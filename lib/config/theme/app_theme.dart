b97c2fec8334ad4cde9f020cbe15290f611c25db
Jopt05 <jopt98@gmail.com>
2023-07-06 12:11:08 -0600

    Audioplayers was added, now user can play the source audios

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -1,6 +1,5 @@
 
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 const List<Color> colorList = [
   Colors.purple,
--- lib/presentation/screens/widgets/home/word_presentation.dart
+++ lib/presentation/screens/widgets/home/word_presentation.dart
@@ -1,28 +1,52 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dictionary_app/infrastructure/models/api_response.dart';
 
-class WordPresentation extends StatelessWidget {
+class WordPresentation extends StatefulWidget {
 
   final String word;
   final List<Phonetic> phonetics;
+  static AudioPlayer player = AudioPlayer();
+
 
   const WordPresentation({
     super.key,
     required this.word,
-    required this.phonetics
+    required this.phonetics,
   });
+
+  @override
+  State<WordPresentation> createState() => _WordPresentationState();
+}
+
+class _WordPresentationState extends State<WordPresentation> {
+
+  static bool isPlaying = false;
+
+  Future<void> playAudio( String source ) async {
+    setState(() {
+      isPlaying = true;
+    });
+    await WordPresentation.player.play(UrlSource(source));
+    Future.delayed(
+      const Duration( seconds: 1 ),
+      () => setState(() {
+        isPlaying = false;
+      }),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    final Phonetic usedPhonetic = phonetics[0];
+    final Phonetic usedPhonetic = widget.phonetics[0];
 
     return Row(
       children: [
         Column(
           children: [
             Text(
-              word.toString().toUpperCase(),
+              widget.word.toString().toUpperCase(),
               style: const TextStyle(
                 fontSize: 42,
                 fontWeight: FontWeight.bold
@@ -39,15 +63,20 @@ class WordPresentation extends StatelessWidget {
           ],
         ),
         const Spacer(),
-        ClipRRect(
-          borderRadius: BorderRadius.circular(30),
-          child: Container(
-            width: 60,
-            height: 60,
-            color: const Color.fromARGB(255, 203, 151, 212),
-            child: const Icon(
-              Icons.play_arrow,
-              color: Colors.purple,
+        if ( usedPhonetic.audio != "" ) GestureDetector(
+          onTap: () {
+            playAudio(usedPhonetic.audio);
+          },
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Container(
+              width: 60,
+              height: 60,
+              color: isPlaying ? Color.fromARGB(255, 133, 76, 143) : const Color.fromARGB(255, 203, 151, 212),
+              child: const Icon(
+                Icons.play_arrow,
+                color: Colors.purple,
+              ),
             ),
           ),
         ),
